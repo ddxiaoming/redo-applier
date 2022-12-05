511948299317c812cd9a05a750f7e5ad9729f525
lemon <1750967031@qq.com>
2022-12-05 10:12:16 +0800

    fix file.h linking compatibility with c++

--- include/storpu/file.h
+++ include/storpu/file.h
@@ -3,10 +3,19 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define FD_HOST_MEM   (-2)
 #define FD_SCRATCHPAD (-3)
 #define FD_FLASH (1)
-ssize_t spu_read(int fd, void* buf, size_t count, unsigned long offset);
-ssize_t spu_write(int fd, const void* buf, size_t count, unsigned long offset);
+ssize_t spu_read(int fd, void *buf, size_t count, unsigned long offset);
+ssize_t spu_write(int fd, const void *buf, size_t count, unsigned long offset);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
